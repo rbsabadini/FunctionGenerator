34b4e9aba01c98e686d04cc389dd30cc564d6ae6
Rafael Sabadini <rbsabadini@hotmail.com>
2018-10-02 23:01:03 +0100

    Ranges of 488Hz - 125KHz, 1.95KHz - 500KHz and 7.812KHz - 2MHz were implemented. For changing between the ranges we have to use the button on the pin RD2 (PORTD)

--- FunctionGenerator.asm
+++ FunctionGenerator.asm
@@ -10,13 +10,21 @@
 		    CBLOCK		H'20'
 		    W_TEMP
 		    STATUS_TEMP
+		    COUNTER
 		    ENDC
 
 ;---- INPUT --------------------------------------------------------------------		    
 		    
 #define		    BUTTON_INC		PORTD, RD0
-#define		    BUTTON_DEC		PORTD, RD1		    
+#define		    BUTTON_DEC		PORTD, RD1
+#define		    BUTTON_F_RANGE	PORTD, RD2
 		    
+;---- OUTPUT --------------------------------------------------------------------		    
+		    
+#define		    LED_PSC16_GREEN	PORTD, RD3
+#define		    LED_PSC4_YELLOW	PORTD, RD4
+#define		    LED_PSC1_RED	PORTD, RD5		    
+		    		    
 ;---- RESET VECTOR -------------------------------------------------------------
 		    ORG			H'0000'
 		    GOTO		setup
@@ -35,14 +43,17 @@
 			
 		    BTFSS		INTCON,2
 		    GOTO		exit_ISR
-		    BCF			INTCON,2
+		    BCF			INTCON,2				
 		    
 		    BTFSS		BUTTON_INC
-		    CALL		inc_PWM
+		    CALL		inc_PWM					;When the button 1 is pressed the signal will increase the frequency of the signal
 		    
 		    BTFSS		BUTTON_DEC
-		    CALL		dec_PWM
+		    CALL		dec_PWM					;When the button 2 is pressed the signal will decrease the frequency
 		    			
+		    BTFSS		BUTTON_F_RANGE
+		    CALL		range_selecting
+		    
 ;----- EXITING INTERRUPT SERVICE ROUTINE ---------------------------------------
 exit_ISR:			
 	
@@ -63,12 +74,82 @@ setup:
 		    CALL		setup_buttons
 		    CALL		setup_PWM
 		    CALL		setup_TMR0
+		    CALL		setup_output
+		    
+		    
+		    MOVLW		H'00'
+		    MOVWF		COUNTER
 		    
 loop:
     
 		    GOTO		$
 
 ;---- SUBROTINE ----------------------------------------------------------------
+
+;------------------------------------------------------------------------------;
+;		    BEGINNING OF THE CODE FOR SQUARE SIGNAL		       ;
+;------------------------------------------------------------------------------;
+range_selecting:
+		
+		    BCF			STATUS, RP0
+		    BCF			STATUS, RP1
+    
+		    INCF		COUNTER
+    
+		    MOVLW		H'04'
+		    XORWF		COUNTER, W
+		    BTFSS		STATUS, Z
+		    GOTO		exit_ISR
+		    
+		    MOVLW		H'00'
+		    MOVWF		COUNTER
+		    
+		    
+		    
+		    MOVLW		H'07'
+		    XORWF		T2CON, W
+		    BTFSC		STATUS, Z
+		    GOTO		PSC_1
+		    
+		    MOVLW		H'04'
+		    XORWF		T2CON, W
+		    BTFSC		STATUS, Z
+		    GOTO		PSC_4
+		    
+		    MOVLW		H'05'
+		    XORWF		T2CON, W
+		    BTFSC		STATUS, Z
+		    GOTO		PSC_16
+		   
+		    RETURN
+		    
+PSC_1:	
+		    MOVLW		H'04'
+		    MOVWF		T2CON
+		    
+		    BCF			LED_PSC16_GREEN
+		    BSF			LED_PSC1_RED
+		    		    
+		    GOTO		exit_ISR
+		    
+PSC_4:	
+		    MOVLW		H'05'
+		    MOVWF		T2CON
+		    
+		    BCF			LED_PSC1_RED
+		    BSF			LED_PSC4_YELLOW
+		    
+		    GOTO		exit_ISR
+		    
+PSC_16:	
+		    MOVLW		H'07'
+		    MOVWF		T2CON
+		    
+		    BCF			LED_PSC4_YELLOW
+		    BSF			LED_PSC16_GREEN
+		    
+		    GOTO		exit_ISR		    
+		    
 		    
 inc_PWM:
 		    BSF			STATUS, RP0
@@ -142,6 +223,10 @@ dec_CCP1CON_CCPR1L:
 		    MOVWF		CCP1CON
     
 		    GOTO		exit_ISR
+;------------------------------------------------------------------------------;
+;		    END OF THE CODE FOR SQUARE SIGNAL			       ;
+;------------------------------------------------------------------------------;		    
+		    
 setup_buttons:
 		    BSF			STATUS, RP0
 		    BCF			STATUS, RP1
@@ -186,8 +271,21 @@ setup_PWM:
 		    MOVLW		H'3E'
 		    MOVWF		CCPR1L
 		    
-		    MOVLW		H'07'
+		    MOVLW		H'07'					; Using prescaler of 16 and turn on the timer2
 		    MOVWF		T2CON
+		    
+		    BSF			LED_PSC16_GREEN
+		    
+		    RETURN
+		    
+setup_output:
+		    BSF			STATUS, RP0
+		    BCF			STATUS, RP1
+		    
+		    MOVLW		H'C7'
+		    MOVWF		TRISD
+			
+		    BCF			STATUS, RP0
 		    
 		    RETURN
 		    
